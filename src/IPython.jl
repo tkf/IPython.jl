63bfd83853ca189d26448bb9768239456ed5963a
Takafumi Arakaki <aka.tkf@gmail.com>
2018-04-28 15:41:53 -0700

    Implement minimal IPython-Julia interaction

--- src/IPython.jl
+++ src/IPython.jl
@@ -1,5 +1,13 @@
 module IPython
 
-# package code goes here
+using PyCall
+
+function start_ipython()
+    pyimport("replhelper")[:customized_ipython]()
+end
+
+function __init__()
+    unshift!(PyVector(pyimport("sys")["path"]), @__DIR__)
+end
 
 end # module
---
+++ src/replhelper.py
@@ -0,0 +1,34 @@
+def jl_name(name):
+    if name.endswith('_b'):
+        return name[:-2] + '!'
+    return name
+
+
+class JuliaNameSpace(object):
+
+    def __init__(self, julia):
+        self.__julia = julia
+
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            super(JuliaNameSpace, self).__setattr__(name, value)
+        else:
+            setter = '(x) -> eval(:({} = $x))'.format(jl_name(name))
+            self.__julia.eval(setter)(value)
+
+    def __getattr__(self, name):
+        if name.startswith('_'):
+            return super(JuliaNameSpace, self).__getattr__(name)
+        else:
+            return self.__julia.eval(jl_name(name))
+
+
+def customized_ipython():
+    import IPython
+    from julia import Julia
+    julia = Julia()
+    Main = JuliaNameSpace(julia)
+    IPython.start_ipython(user_ns=dict(
+        julia=julia,
+        Main=Main,
+    ))
