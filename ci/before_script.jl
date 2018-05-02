b509c7bfbf7a7881f7f92fe0f36edeed371988f6
Takafumi Arakaki <aka.tkf@gmail.com>
2018-05-02 13:47:03 -0700

    Install pyjulia in ci/before_script.jl

--- ci/before_script.jl
+++ ci/before_script.jl
@@ -12,5 +12,10 @@ info("Conda.add(ipython)")
 using Conda
 Conda.add("ipython")
 
+using PyCall
+install_pyjulia = `$(PyCall.pyprogramname) -m pip install julia`
+info(install_pyjulia)
+run(install_pyjulia)
+
 info("show_versions.jl")
 include("show_versions.jl")
