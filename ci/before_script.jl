16b65b24d2ce0582d75c854ec8e6aed083dd8115
Takafumi Arakaki <aka.tkf@gmail.com>
2018-05-12 00:41:10 -0700

    Reduce explicit installations in before_script.jl

--- ci/before_script.jl
+++ ci/before_script.jl
@@ -9,8 +9,6 @@ info("Pkg.build(IPython)")
 Pkg.build("IPython")
 
 using IPython
-IPython.install_dependency("ipython"; force=true)
-IPython.install_dependency("julia"; force=true)
 IPython.install_dependency("pytest"; force=true)
 
 info("show_versions.jl")
