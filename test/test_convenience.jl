d0f6d4afb62311847f988d16bf5564f1ee518add
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-20 16:45:02 -0700

    Don't check if PyJulia is installed

--- test/test_convenience.jl
+++ test/test_convenience.jl
@@ -4,14 +4,12 @@ include("preamble.jl")
 
 @testset "Convenience" begin
     @test_nothrow IPython.envinfo(devnull)
-    @test IPython.pyversion("julia") isa String
     @test IPython.pyversion("IPython") isa String
     @test IPython.pyversion("IPython") ==  IPython._pyversion("IPython")
     @test IPython.pyversion("__NON_EXISTING__") isa Nothing
 
     println("vvv DRY RUN vvv")
     @test_nothrow IPython.install_dependency("ipython"; dry_run=true)
-    @test_nothrow IPython.install_dependency("julia"; dry_run=true)
     @test_nothrow IPython.install_dependency("spam"; dry_run=true)
     println("^^^ DRY RUN ^^^")
 
