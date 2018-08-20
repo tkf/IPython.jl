9bc62fa41e65b01771411763c9c4818f8bc4b5e7
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-20 00:26:13 -0700

    Test against Julia 0.7 and 1.0

--- ci/before_script.jl
+++ ci/before_script.jl
@@ -1,11 +1,19 @@
+@static if VERSION >= v"0.7.0-"
+    using Pkg
+else
+    macro info(x)
+        :(info($(esc(x))))
+    end
+end
+
 # Let PyCall.jl use Python interpreter from Conda.jl
 # See: https://github.com/JuliaPy/PyCall.jl
 ENV["PYTHON"] = ""
 
-info("Pkg.clone(pwd())")
+@info "Pkg.clone(pwd())"
 Pkg.clone(pwd())
 
-info("Pkg.build(IPython)")
+@info "Pkg.build(IPython)"
 Pkg.build("IPython")
 
 using IPython
@@ -13,5 +21,5 @@ IPython.install_dependency("ipython"; force=true)
 IPython.install_dependency("julia"; force=true)
 IPython.install_dependency("pytest"; force=true)
 
-info("show_versions.jl")
+@info "show_versions.jl"
 include("show_versions.jl")
