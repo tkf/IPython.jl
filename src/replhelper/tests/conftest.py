897207ed5ddc6174988307cca1cafb039d73450b
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-30 01:15:23 -0700

    Fix Main fixture

--- src/replhelper/tests/conftest.py
+++ src/replhelper/tests/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 
-from ..core import get_main
+from ..core import get_cached_main
 
 
 @pytest.fixture
 def Main():
     """ pytest fixture for providing a Julia `Main` name space. """
-    Main = get_main()
+    Main = get_cached_main()
     if Main is None:
         pytest.skip("Main not configured (not called from IPython.jl?)")
     else:
