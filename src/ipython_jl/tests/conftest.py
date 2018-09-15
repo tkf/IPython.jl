991ccb37773d6d8a800a3d9b797654b74a151aad
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-15 02:38:01 -0700

    Make all fixtures session-scoped (they should be)

--- src/ipython_jl/tests/conftest.py
+++ src/ipython_jl/tests/conftest.py
@@ -4,7 +4,7 @@ from .. import core
 from .. import ipyext
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def Main():
     """ pytest fixture for providing a Julia `Main` name space. """
     if core._Main is None:
@@ -13,15 +13,14 @@ def Main():
         return core._Main
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def julia(Main):
     """ pytest fixture for providing a `JuliaAPI` instance. """
     return core.get_api(Main)
 
 
 @pytest.fixture(scope="session")
-def ipy_with_magic():
-    Main()  # check if Julia API is ready
+def ipy_with_magic(Main):
     from IPython.testing.globalipapp import get_ipython
     ip = get_ipython()
     ipyext.load_ipython_extension(ip)
