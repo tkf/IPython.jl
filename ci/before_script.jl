933a7b08c24207e0dc593f1a2ddf05794417237a
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-27 23:03:12 -0700

    Print PyCall's build.log in ci/before_script.jl

--- ci/before_script.jl
+++ ci/before_script.jl
@@ -17,6 +17,14 @@ Pkg.clone(pwd())
 Pkg.build("IPython")
 
 using IPython
+
+if VERSION >= v"0.7.0-"
+    @info "PyCall/deps/build.log:"
+    print(read(
+        joinpath(dirname(dirname(pathof(IPython.PyCall))), "deps", "build.log"),
+        String))
+end
+
 IPython.install_dependency("pytest"; force=true)
 IPython.install_dependency("ipython"; force=true)
 if get(ENV, "CONDA_JL_VERSION", "") != "2"
