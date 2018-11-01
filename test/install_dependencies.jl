dd9a82795033a9657f5323dfa2c44c63c5c42d94
Takafumi Arakaki <aka.tkf@gmail.com>
2018-11-01 14:25:00 -0700

    Rebuild PyCall after installing ipython

--- test/install_dependencies.jl
+++ test/install_dependencies.jl
@@ -12,16 +12,20 @@ Pkg.build("PyCall")
 using Compat: @info
 using IPython
 
-if VERSION >= v"0.7.0-"
-    @info "PyCall/deps/build.log:"
-    print(read(
-        joinpath(dirname(dirname(pathof(IPython.PyCall))), "deps", "build.log"),
-        String))
-end
-
 IPython.install_dependency("pytest"; force=true)
 IPython.install_dependency("ipython"; force=true)
 if get(ENV, "CONDA_JL_VERSION", "") == "2"
     # For IPython.testing.globalipapp
     IPython.install_dependency("mock"; force=true)
+end
+
+# Build PyCall again, since above installation could have changed
+# Python versions.
+Pkg.build("PyCall")
+
+if VERSION >= v"0.7.0-"
+    @info "PyCall/deps/build.log:"
+    print(read(
+        joinpath(dirname(dirname(pathof(IPython.PyCall))), "deps", "build.log"),
+        String))
 end
