113bbc9dddfde69d45efeda4f14036902b2e6cb0
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-23 23:55:06 -0800

    Support getproperty-based PyCall API

--- src/core.jl
+++ src/core.jl
@@ -1,12 +1,19 @@
 using PyCall
 import Conda
 
+const _getproperty = try
+    pyimport("sys").executable
+    getproperty
+catch
+    getindex
+end
+
 julia_exepath() =
     joinpath(VERSION < v"0.7.0-DEV.3073" ? JULIA_HOME : Base.Sys.BINDIR,
              Base.julia_exename())
 
 function _start_ipython(name; kwargs...)
-    pyimport("ipython_jl")[name](;
+    _getproperty(pyimport("ipython_jl"), name)(;
         eval_str = JuliaAPI.eval_str,
         api = JuliaAPI,
         kwargs...)
@@ -17,6 +24,6 @@ function start_ipython(; kwargs...)
 end
 
 function __init__()
-    pushfirst!(PyVector(pyimport("sys")["path"]), @__DIR__)
+    pushfirst!(PyVector(_getproperty(pyimport("sys"), "path")), @__DIR__)
     afterreplinit(init_repl)
 end
